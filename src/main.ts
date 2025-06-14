7652e83cfc89ed899d591fa38e60ba81f621e4e8
Gilmer J. Vizconde <gilmerjose.vizconde2@gmail.com>
2025-06-14 19:49:23 -0400

    feat: broadcast messages to all users

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,13 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     <span id="server-status">offline</span>    
 
   <ul id="clients-ul"></ul>
+
+  <form id="message-form">
+  <input placeholder="message" id="message-input"/>
+  </form>
+
+  <h3>Messages</h3>
+  <ul id="messages-ul"></ul>
   
   </div>
 `;
--- src/socket-client.ts
+++ src/socket-client.ts
@@ -9,8 +9,11 @@ export const connectToServer = () => {
 };
 
 const addListerners = (socket: Socket) => {
-  const serverStatusLabel = document.querySelector('#server-status')!;
   const clientUl = document.querySelector('#clients-ul')!;
+  const messageForm = document.querySelector<HTMLFormElement>('#message-form')!;
+  const messageInput = document.querySelector<HTMLInputElement>('#message-input')!;
+  const messagesUl = document.querySelector<HTMLUListElement>('#messages-ul')!;
+  const serverStatusLabel = document.querySelector('#server-status')!;
 
   socket.on('connect', () => {
     serverStatusLabel.innerHTML = 'connected';
@@ -28,5 +31,30 @@ const addListerners = (socket: Socket) => {
       `;
     });
     clientUl.innerHTML = clientsHtml;
+  });
+
+  messageForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    if (messageInput.value.trim().length <= 0) return;
+
+    socket.emit('message-from-client', {
+      id: 'YO!!',
+      message: messageInput.value
+    });
+
+    messageInput.value = '';
+  });
+
+  socket.on('message-from-server', (payload: { fullName: string; message: string }) => {
+    const newMessage = `
+    <li>
+        <strong>${payload.fullName}</strong>
+        <span>${payload.message}</span>
+    </li>
+    `;
+    const li = document.createElement('li');
+    li.innerHTML = newMessage;
+
+    messagesUl.append(li);
   });
 };
